92818b8613c966257e46e786f4fe697d770b9f2e
Innei <tukon479@gmail.com>
2019-11-03 17:26:00 +0800

    bubble.

---
+++ src/components/form/bubbleNotice.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="notice-list">
+    <div class="bubble" :style="{ backgroundColor: color}">
+      <div class="content">{{msg}}</div>
+    </div>
+    <div class="bubble" :style="{ backgroundColor: color}">
+      <div class="content">{{msg}}</div>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msgList: []
+  },
+  data () {
+    return {
+      msg: '我好了',
+      color: '#e3e3e3'
+    }
+  }
+}
+</script>
+
+<style>
+.notice-list {
+  top: 2em;
+  left: 0;
+  right: 0;
+  z-index: 60;
+  position: fixed;
+  user-select: none;
+  pointer-events: none;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+}
+.bubble {
+  color: #fff;
+  display: table;
+  background: #333;
+  border-radius: 3em;
+  pointer-events: auto;
+  margin: 0 auto 1em auto;
+  box-shadow: 0 5px 5px -2px rgba(0, 0, 0, 0.2);
+}
+.bubble .content {
+  padding: 0.5em 1em;
+  display: table-cell;
+}
+</style>
--- src/views/Init.vue
+++ src/views/Init.vue
@@ -47,12 +47,18 @@
         </form>
       </transition>
     </main>
+
+    <bubble/>
   </div>
 </template>
 
 <script>
 import masterApi from '@/api/master'
+import Bubble from '@/components/form/bubbleNotice.vue'
 export default {
+  components: {
+    Bubble
+  },
   data () {
     return {
       postUrl: process.env.development ? 'http://localhost:3000/api/master/first_init' : '/api/master/first_init',
