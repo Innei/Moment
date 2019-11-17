5b51243fd46acb0943b96b587b9dedd1afbcaa7a
Innei <tukon479@gmail.com>
2019-11-17 18:03:51 +0800

    user profile.

--- src/views/Admin/analyse/chart.vue
+++ src/views/Admin/analyse/chart.vue
@@ -1,8 +1,11 @@
 <template>
   <layout>
     <template #main>
-      <h1>最近 7 天 访客数据</h1>
-      <ve-line :data="PVchartData" :settings="chartSettings"></ve-line>
+      <!-- <h1>最近 7 天 访客数据</h1> -->
+      <div class="chart">
+        <ve-line :title="title" :loading="loading" :data="PVchartData" :settings="chartSettings" />
+      </div>
+
       <!-- <ve-pie :data="WeekChartData"></ve-pie> -->
     </template>
   </layout>
@@ -10,8 +13,12 @@
 
 <script>
 import VeLine from 'v-charts/lib/line.common'
-// import VePie from 'v-charts/lib/pie.common'
+// import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/component/title'
+import 'v-charts/lib/style.css'
+
 import { getAnalytics } from '@/api/options'
+// import VePie from 'v-charts/lib/pie.common'
 export default {
   components: {
     layout: () => import('@/components/Admin/layout.vue'),
@@ -22,37 +29,41 @@ export default {
     if (data.ok === 1) {
       // const today = Date.now()
       this.PVchartData.rows = data.weekNum
+      this.loading = false
       // this.WeekChartData.rows = [{'本周 PV': data.week, '全年 PV': data.year}]
     }
   },
   data () {
-    this.chartSettings = {
-      labelMap: {
-        PV: 'PV',
-        day: '日期',
-        IP: 'IP'
-      }
-    }
     return {
       PVchartData: {
         columns: ['day', 'PV', 'IP'],
         rows: []
       },
-      // WeekChartData: {
-      //   columns: ['本周 PV', '全年 PV'],
-      //   rows: []
-      // }
+      chartSettings: {
+        labelMap: {
+          PV: 'PV',
+          day: '日期',
+          IP: 'IP'
+        }
+      },
+      title: {
+        text: '最近 7 天 访客数据',
+        // left: 'center'
+      },
+      loading: true
     }
   }
-
-
 }
 </script>
 
 <style scoped>
-h1 {
+/* h1 {
   text-align: center;
   font-weight: 400;
   font-size: 1.2rem;
-}
+} */
+/* .chart {
+  display: flex;
+  justify-content: center;
+} */
 </style>
--- src/views/Admin/setting/profile.vue
+++ src/views/Admin/setting/profile.vue
@@ -1,18 +1,76 @@
 <template>
   <layout>
     <template #main>
-      
+      <div class="grid">
+        <div class="left">
+          <img
+            class="before"
+            @load="$refs.avatar.classList.remove('before')"
+            @error="$refs.avatar.classList.add('before')"
+            :src="user.avatar"
+            ref="avatar"
+          />
+          <div class="add">+</div>
+        </div>
+        <div class="right">
+          <form action="#"></form>
+        </div>
+      </div>
     </template>
   </layout>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   components: {
     layout: () => import('@/components/Admin/layout.vue')
+  },
+  computed: {
+    ...mapGetters(['user'])
+  },
+  methods: {
+    ...mapActions(['loadUser'])
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 250px auto;
+}
+.left, .right {
+  position: relative;
+}
+img {
+  max-width: 100px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  border: 3px solid #eee;
+  transition: border 0.8s;
+  transform: scale(2);
+  transform-origin: left top;
+  margin: 0 50px;
+  position: relative;
+}
+img.before {
+  width: 50px;
+  height: 50px;
+  border: 53px solid #eee;
+}
+img:not([before]) + .add {
+  border-radius: 50%;
+  background: #00A0D7;
+  display: inline-block;
+  position: absolute;
+  bottom: -50%;
+  right: 10px;
+  height: 2rem;
+  width: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+}
 </style>
