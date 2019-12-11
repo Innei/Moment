e5b5cfb9d08564cb4e7aaa044c18b9175712b818
Innei <tukon479@gmail.com>
2019-12-11 14:43:37 +0800

    split

--- src/router.js
+++ src/router.js
@@ -16,63 +16,90 @@ const router = new Router({
     {
       name: 'init',
       path: '/init',
-      component: () => import('@/views/Init.vue'),
+      component: () =>
+        import(/* webpackChunkName: "init" */ '@/views/Init.vue'),
       meta: { once: true, title: 'おかえりなさいご主人様 -- Moment' }
     },
     {
       name: 'login',
       path: '/login',
-      component: () => import('@/views/Login.vue'),
+      component: () =>
+        import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
       meta: { title: '登陆 -- Moment' }
     },
     {
       name: 'admin',
       path: '/master',
       meta: { private: true, title: ' -- Moment' },
-      component: () => import('@/views/Admin/index.vue'),
+      component: () =>
+        import(/* webpackChunkName: "master" */ '@/views/Admin/index.vue'),
       redirect: '/master/dashboard',
       children: [
         {
           path: 'dashboard',
-          component: () => import('@/views/Admin/dashboard.vue'),
+          component: () =>
+            import(
+              /* webpackChunkName: "dashboard" */ '@/views/Admin/dashboard.vue'
+            ),
           meta: { title: '仪表盘' }
         },
         {
           path: 'moments',
-          component: () => import('@/views/Admin/manageMoment.vue'),
+          component: () =>
+            import(
+              /* webpackChunkName: "manageMoment" */ '@/views/Admin/manageMoment.vue'
+            ),
           meta: { title: '管理瞬间' }
         },
         {
           path: 'analytics',
-          component: () => import('@/views/Admin/setting/index.vue'),
+          component: () =>
+            import(
+              /* webpackChunkName: "analyse" */ '@/views/Admin/setting/index.vue'
+            ),
           redirect: '/master/analytics/chart',
           children: [
             {
               path: 'chart',
               meta: { title: '图表' },
-              component: () => import('@/views/Admin/analyse/chart.vue')
+              component: () =>
+                import(
+                  /* webpackChunkName: "chart" */ '@/views/Admin/analyse/chart.vue'
+                )
             },
             {
               path: 'table',
               meta: { title: '访客数据' },
-              component: () => import('@/views/Admin/analyse/table.vue')
+              component: () =>
+                import(
+                  /* webpackChunkName: "table" */ '@/views/Admin/analyse/table.vue'
+                )
             }
           ]
         },
         {
           path: 'setting',
-          component: () => import('@/views/Admin/setting/index.vue'),
+          component: () =>
+            import(
+              /* webpackChunkName: "setting" */ '@/views/Admin/setting/index.vue'
+            ),
           redirect: '/master/setting/profile',
           children: [
             {
               path: 'profile',
               meta: { title: '主人信息' },
-              component: () => import('@/views/Admin/setting/profile.vue')
+              component: () =>
+                import(
+                  /* webpackChunkName: "profile" */ '@/views/Admin/setting/profile.vue'
+                )
             },
             {
               path: 'reset',
               meta: { title: '修改密码' },
-              component: () => import('@/views/Admin/setting/reset.vue')
+              component: () =>
+                import(
+                  /* webpackChunkName: "reset" */ '@/views/Admin/setting/reset.vue'
+                )
             }
           ]
         }
